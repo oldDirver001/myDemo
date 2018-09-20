06bc1280016702a19af0ecebd655bdfdbf6eb0b7
DESKTOP-1330E0N\87227 <872277350@qq.com>
2018-09-20 09:23:02 +0800

    第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-transfer v-model="value1" :data="data"></el-transfer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    const generateData = _ => {
+      const data = []
+      for (let i = 1; i <= 15; i++) {
+        data.push({
+          key: i,
+          label: `备选项 ${i}`,
+          disabled: i % 4 === 0
+        })
+      }
+      return data
+    }
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      data: generateData(),
+      value1: [1, 4]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-transfer v-model="value1" :data="data"></el-transfer>
+  </div>
+</template>
+<script>
+export default {
+  name: 'test',
+  data() {
+    const generateData = _ => {
+      const data = []
+      for (let i = 1; i <= 15; i++) {
+        data.push({
+          key: i,
+          label: `备选项 ${i}`,
+          disabled: i % 4 === 0
+        })
+      }
+      return data
+    }
+    return {
+      data: generateData(),
+      value1: [1, 4]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped></style>
